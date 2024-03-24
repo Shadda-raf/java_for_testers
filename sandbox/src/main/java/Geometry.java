8689d346569b7ba853ef41b843135ea87a34ba66
Shadda-raf <borislav3001@gmail.com>
2024-03-24 23:04:50 +0300

    Добавлен класс Geometry

---
+++ sandbox/src/main/java/Geometry.java
@@ -0,0 +1,9 @@
+public class Geometry {
+    public static void main(String[] args) {
+        var side=7;
+        System.out.println("Введите длину стороны квадрата");
+      //  System.in.read(s);
+        System.out.println("Площадь квадрата = " + (side*side));
+
+    }
+}
