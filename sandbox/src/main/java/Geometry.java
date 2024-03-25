8e7545e91e92fe3a34000103e08c4da98cdf532a
Shadda-raf <borislav3001@gmail.com>
2024-03-25 22:41:44 +0300

    Эксперименты с функциями

--- sandbox/src/main/java/Geometry.java
+++ sandbox/src/main/java/Geometry.java
@@ -1,9 +1,27 @@
 public class Geometry {
     public static void main(String[] args) {
-        var side=7;
-        System.out.println("Введите длину стороны квадрата");
-      //  System.in.read(s);
-        System.out.println("Площадь квадрата = " + (side*side));
 
+        printSquareArea(7.0);
+        printSquareArea(5.0);
+        printSquareArea(3.0);
+        
+        printRectangleArea(3.7 , 5.4);
+
+    }
+
+    private static void printRectangleArea(double a, double b) {
+        System.out.println("Площадь прямоугольника  со сторонами a =  " + a +" и b = " + b + " = "+ rectangleArea(a,b));
+    }
+
+    private static double rectangleArea(double a, double b) {
+        return a*b;
+    }
+
+    static public void printSquareArea(double side) {
+        System.out.println("Площадь квадрата со стороной  " + side +" = "+ squareArea(side));
+    }
+
+    private static double squareArea (double a) {
+        return a * a;
     }
 }
