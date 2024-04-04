3fba8e3c9aece009525f8983baf8546fb2a8d915
Shadda-raf <borislav3001@gmail.com>
2024-04-05 00:02:33 +0300

    Homework 3

--- sandbox/src/main/java/ru/stqa/Geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/Geometry/figures/Triangle.java
@@ -1,7 +1,15 @@
 package ru.stqa.Geometry.figures;
 
 public record Triangle(double sideA,double sideB, double sideC) {
+    public Triangle{
+        if (sideA<0 || sideB<0 || sideC<0) {
+            throw new IllegalArgumentException("Triangle side must be non-negative!");
+        }
 
+        if ((sideA + sideB) < sideC || (sideA + sideC) < sideB || (sideB + sideC) < sideA){
+            throw new IllegalArgumentException("Triangle inequality violated!");
+        }
+    }
 
     public double area() {
         return this.formulaGerona();
--- sandbox/src/test/java/ru/stqa/Geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/Geometry/figures/TriangleTests.java
@@ -2,6 +2,7 @@ package ru.stqa.Geometry.figures;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.function.Try;
 
 public class TriangleTests {
 
@@ -18,4 +19,25 @@ public class TriangleTests {
         double perimeterResult= t.perimeter();
         Assertions.assertEquals(37.9999999, perimeterResult);
     }
+
+    @Test
+    void cannotCreateTriangleWithNegativeSide() {
+        try {
+            new Triangle(-70.0, 5.0,7.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception){
+            //ok
+        }
+    }
+
+    @Test
+    void cannotCreateTriangleWithTheseSideCombination() {
+        try {
+            new Triangle(17.0, 5.0,7.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception){
+            //ok
+        }
+    }
 }
+
