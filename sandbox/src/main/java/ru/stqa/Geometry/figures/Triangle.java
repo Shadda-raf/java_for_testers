9c9b78ce62dd997048cb48021b94ab3e9a0a3671
Shadda-raf <borislav3001@gmail.com>
2024-05-11 23:15:16 +0300

    еще одна доработка сравнений

--- sandbox/src/main/java/ru/stqa/Geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/Geometry/figures/Triangle.java
@@ -1,6 +1,8 @@
 package ru.stqa.Geometry.figures;
 
-public record Triangle(double sideA,double sideB, double sideC) {
+import java.util.Objects;
+
+public record Triangle(double sideA, double sideB, double sideC) {
     public Triangle{
         if (sideA<0 || sideB<0 || sideC<0) {
             throw new IllegalArgumentException("Triangle side must be non-negative!");
@@ -20,9 +22,29 @@ public record Triangle(double sideA,double sideB, double sideC) {
         double semiperimetr = this.perimeter() / 2;
         return Math.sqrt((semiperimetr) *
                 (semiperimetr - this.sideA) *
+                
                 (semiperimetr - this.sideB) *
                 (semiperimetr - this.sideC)
         );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return Double.compare(sideA, triangle.sideA) == 0 && Double.compare(sideB, triangle.sideB) == 0 && Double.compare(sideC, triangle.sideC) == 0
+                || Double.compare(sideA, triangle.sideB) == 0 && Double.compare(sideB, triangle.sideC) == 0 && Double.compare(sideC, triangle.sideA) == 0
+                ||  Double.compare(sideA, triangle.sideC) == 0 && Double.compare(sideB, triangle.sideA) == 0 && Double.compare(sideC, triangle.sideB) == 0
+                ||  Double.compare(sideA, triangle.sideC) == 0 && Double.compare(sideB, triangle.sideB) == 0 && Double.compare(sideC, triangle.sideA) == 0
+                ||  Double.compare(sideA, triangle.sideB) == 0 && Double.compare(sideB, triangle.sideA) == 0 && Double.compare(sideC, triangle.sideC) == 0;
+
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sideA, sideB, sideC);
     }
 
     public double perimeter() {
--- sandbox/src/test/java/ru/stqa/Geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/Geometry/figures/TriangleTests.java
@@ -38,6 +38,21 @@ public class TriangleTests {
         } catch (IllegalArgumentException exception){
             //ok
         }
+
+    }
+
+    @Test
+
+    void triangleABCEqualsABC(){
+        var t1 = new Triangle(3.0, 4.0 , 5.0);
+        var t2 = new Triangle(3.0, 4.0, 5.0);
+        Assertions.assertEquals(t1 , t2);
+    }
+
+    void triangleABCEqualsBCA(){
+        var t1 = new Triangle(3.0, 4.0 , 5.0);
+        var t2 = new Triangle(4.0, 5.0, 3.0);
+        Assertions.assertEquals(t1 , t2);
     }
 }
 
